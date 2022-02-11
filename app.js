ba05ddeea1f84325f41ca53e4fea9e119982b2b1
Mehmet Ali BÜBER <mehmetalibuber6@gmail.com>
2022-02-11 17:27:02 +0300

    class yapısı ve kalıtım

--- app.js
+++ app.js
@@ -47,3 +47,67 @@ Woman.prototype.myTest = function () {
 const miras = new Woman("Name", 10, 1000);
 console.log(miras);
 miras.test1();
+
+
+// Class yapısını kullanma ve constructor oluşturma
+
+class Employee2 {
+    constructor(name, age, salary) { //create a constructor function. Don't need to write "function"
+        this.name = name,
+            this.age = age,
+            this.salary = salary
+    }
+    showInfo2() {
+        console.log("showInfo2 aktif edildi");
+    }
+}
+
+const emp = new Employee2("Mehmet", 23, 6000);
+console.log(emp);
+emp.showInfo2();
+
+
+class Code {
+    html() {
+        console.log("html working");
+    }
+    static js() {
+        console.log("js working");
+    }
+}
+//working together
+const emp3 = new Code();
+emp3.html();
+Code.js();
+//emp3.js(); this is a wrong usages * is not a function error
+
+// ES6 class create
+class Person{ //SuperClass - BaseClass
+    constructor(name, age){
+        this.name = name,
+        this.age = age
+    }
+    showInfo3(){
+        console.log("showInfo3 working");
+    }
+}
+class Employee3 extends Person{ // Derived - SubClass - ChildClass
+    constructor(name, age, salary){
+        super(name,age),
+        this.salary = salary
+    }
+    showInfo3(){
+        console.log("showInfo3 working in base prototype");
+    }
+    toString(){
+        console.log("Employee3");
+    }
+    Increase(amount){
+        this.salary += amount;
+    }
+}
+const emp4 = new Employee3("Mehmet", 23, 6200);
+console.log(emp4);
+emp4.showInfo3();
+emp4.toString();
+emp4.Increase(100);
